4ffa0be688a2fe93acfc73c9542c00a275d4b908
guru28272 <117896953+guru28272@users.noreply.github.com>
2023-03-16 12:00:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,3 +1,21 @@
 const url = "https://jsonplaceholder.typicode.com/posts/1";
 
 //your JS code here. If required.
+
+const btn = document.querySelector(`#btn`);
+const output = document.querySelector(`#output`);
+
+btn.addEventListener(`click`, getData());
+
+//your JS code here. If required.
+async function getData() {
+  const res = await fetch(url);
+  //   console.log(res);
+  const titleData = await data(res);
+  output.textContent = titleData.title;
+}
+
+async function data(res) {
+  const data = await res.json();
+  return data;
+}
